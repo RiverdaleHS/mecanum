5e51964e97ce020fa92ed7d62a96ef7593e1e0d0
Henry O'Scannlain-Miller <Henryom@me.com>
2018-11-07 10:39:49 -0800

    added set motors

--- src/main/java/frc/robot/commands/JoystickDrive.java
+++ src/main/java/frc/robot/commands/JoystickDrive.java
@@ -25,6 +25,11 @@ public class JoystickDrive extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    double virtical = Robot.m_oi.stick.getY();
+    double horizontal = Robot.m_oi.stick.getX();
+    double twist = Robot.m_oi.stick.getTwist();
+    //if this works the first time lol
+    Robot.m_Chassis.setMotors(virtical - twist - horizontal, virtical - twist + horizontal, virtical + twist - horizontal, virtical + twist + horizontal);
   }
 
   // Make this return true when this Command no longer needs to run execute()
