bdedef4a7e70ed1ef118126383d3b61ea097a8dd
Henry O'Scannlain-Miller <Henryom@me.com>
2018-12-11 16:34:38 -0800

    message

--- src/main/java/frc/robot/commands/JoystickDrive.java
+++ src/main/java/frc/robot/commands/JoystickDrive.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 public class JoystickDrive extends Command {
+  
   public JoystickDrive() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
